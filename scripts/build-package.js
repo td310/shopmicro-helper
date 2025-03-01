94f427dbfabf56833abf581de5d5450e75972467
Tai <ndtai0912@gmail.com>
2025-03-01 14:30:29 +0700

    update helper

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -1,13 +1,3 @@
-const fs = require('fs');
-const pkg = require('../package.json');
+const { copyFileSync } = require('fs');
 
-pkg.main = './cjs/index.js';
-pkg.module = './esm/index.js';
-pkg.types = './src/index.d.ts';
-pkg.exports['.'] = {
-  require: './cjs/index.js',
-  import: './esm/index.js',
-  types: './src/index.d.ts'
-};
-
-fs.writeFileSync('build/package.json', JSON.stringify(pkg, null, 2));
+copyFileSync('package.json', 'build/package.json');
